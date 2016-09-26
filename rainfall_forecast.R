551cd724b02ada1ead201a6de950d9e9c6239ef9
Chiam Cheng <chiamcheng@gmail.com>
2016-09-26 16:07:58 +1000

    data analaysing - STL models and specific dates

--- rainfall_forecast.R
+++ rainfall_forecast.R
@@ -2,13 +2,23 @@ rates <- read.csv("clean_rainfall.csv",header = T)
 rates$date <- as.Date(rates$date, "%Y-%m-%d")
 str(rates$date)
 range(rates$date)
-plot(rates$date, rates$rainfall_amount, type = "l")
+plot(rates$date, rates$rainfall_amount, type = "c")
 head(rates$date, 20)
 years <- format(rates$date, "%Y")
 tab <- table(years)
 tab
 mean(tab[1:(length(tab) - 1)])
 source("forecast.R")  ## see code file in section 5
-result.arima <- forecastArima(rates, n.ahead = 20)
 source("plotForecastResult.R")  ## see code file in section 5
-plotForecastResult(result.arima, title = "Rainfall forecasting with ARIMA")
+
+# ARIMA MODEL HERE
+# result.arima <- forecastArima(rates, n.ahead = 30)
+# plotForecastResult(result.arima, title = "Rainfall forecasting with ARIMA")
+
+# STL MODEL HERE
+# result.stl <- forecastStl(rates, n.ahead = 90)
+# plotForecastResult(result.stl, title = "Exchange rate forecasting with STL")
+
+# STL MODEL WITH DATES
+result <- subset(result.stl, date >= "2014-01-01")
+plotForecastResult(result, title = "Exchange rate forecasting with STL (2014)")
